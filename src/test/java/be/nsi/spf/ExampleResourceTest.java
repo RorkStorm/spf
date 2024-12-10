de068b97b137a80a52e6a92c4854e412ed9c5d39
Eric Fonteyne <eric.fonteyne@gmail.com>
2024-12-10 13:19:22 +0100

    UT Failed

--- src/test/java/be/nsi/spf/ExampleResourceTest.java
+++ src/test/java/be/nsi/spf/ExampleResourceTest.java
@@ -2,7 +2,7 @@ package be.nsi.spf;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -21,6 +21,6 @@ class ExampleResourceTest {
     @Test
     public void testShouldReturnFalse() {
         boolean result = false;
-        assertFalse(result, "The method should return false");
+        assertTrue(result, "The method should return false");
     }
 }
